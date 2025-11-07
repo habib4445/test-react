9e335aefb1aa04c5012e62b998202cb7a7aec04d
habib4445 <0124habibur@gmail.com>
2025-11-07 21:22:32 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,22 @@ function App() {
 function handleClick(){
   alert('I am clicker');
 }
+const handleClick3=()=>{
+  alert('I am clicked 3')
+}
+
+const handleAdd5 =(num)=>{
+  const newNum=num+5;
+  alert(newNum);
+}
+
   return (
     <>
       <h1>Vite + React</h1>
 <button onClick={handleClick} >Click Me</button>
+<button onClick={handleClick3} >Click Me2</button>
+<button onClick={()=> alert('clicked 4')} >Click Me4</button>
+<button onClick={() => handleAdd5(10)} > Add 5</button>
 
     </>
   );
